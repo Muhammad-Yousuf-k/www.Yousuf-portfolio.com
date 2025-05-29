0f34879676585647b9fedeaded022cb278026e23
Muhammad-Yousuf-k <muhammad.yousuf8166@gmail.com>
2025-05-29 17:43:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ projects.forEach((project) => {
  <img src="${project.image}" alt="${project.title}">
  <h3>${project.title}</h3>
  <p>${project.description}</p>
- <a href="${project.link}">
+ <a href="${project.link}" target="_blank">
  <button>view project</button>
  </a>
  `;
